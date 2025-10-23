64c67cf6579d39f8058e0bfa39a677178e66036c
Antouto <anthonydsmart@gmail.com>
2025-10-23 07:44:30 +0200

    .

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,8 +1,13 @@
-import type { CommandInteraction } from "dressed";
+import { createMessage, type CommandInteraction } from "dressed";
  
 export default async function (interaction: CommandInteraction) {
-  await interaction.reply({
-    content: "test",
-    ephemeral: true
+  await interaction.deferReply({ ephemeral: true })
+
+  await createMessage(interaction.channel.id, "1")
+  await createMessage(interaction.channel.id, "2")
+  await createMessage(interaction.channel.id, "3")
+
+  await interaction.editReply({
+    content: "test"
   });
 }
